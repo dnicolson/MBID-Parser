8edca2a3ac169828b853848b3c4cc36bda233717
David Nicolson <david.nicolson@gmail.com>
2016-05-16 18:07:13 +0200

    :cop:

--- MBID.c
+++ MBID.c
@@ -35,8 +35,8 @@ EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #ifndef __NT__
 #include <sys/errno.h>
 #endif
@@ -44,38 +44,41 @@ EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "MBID.h"
 
 #ifdef MBID_DEBUG
-#define debug(fmt, ...) printf(fmt, ## __VA_ARGS__)
+#define debug(fmt, ...) printf(fmt, ##__VA_ARGS__)
 #else
 #define debug(fmt, ...)
 #endif
 
-
-int to_syncsafe(char bytes[]) {
+int to_syncsafe(char bytes[])
+{
     return ((int)bytes[0] << 21) + ((int)bytes[1] << 14) + ((int)bytes[2] << 7) + (int)bytes[3];
 }
 
-int to_integer(char bytes[]) {
+int to_integer(char bytes[])
+{
     int size = 0;
     int i;
-    for (i=0; i<4; i++) {
+    for (i = 0; i < 4; i++) {
         size = size * 256 + ((int)bytes[i] & 0x000000FF);
     }
     return size;
 }
 
-int mfile(int length, char ret[], FILE *fp) {
-    size_t bytes = fread(ret,1,length,fp);
-    
+int mfile(int length, char ret[], FILE* fp)
+{
+    size_t bytes = fread(ret, 1, length, fp);
+
     if (bytes == length) {
         return 0;
     } else {
         return -1;
     }
 }
-    
-int parse_mbid(const char *path, char mbid[MBID_BUFFER_SIZE]) {
 
-    FILE *fp;
+int parse_mbid(const char* path, char mbid[MBID_BUFFER_SIZE])
+{
+
+    FILE* fp;
     int s = 1;
     char head[3];
     char version[2];
@@ -94,112 +97,121 @@ int parse_mbid(const char *path, char mbid[MBID_BUFFER_SIZE]) {
         return -1;
     }
 
-    fp = fopen(path,"rb");
+    fp = fopen(path, "rb");
     if (fp == NULL) {
-        debug("Could not open file: %s\n",path);
+        debug("Could not open file: %s\n", path);
         return -1;
     }
 
     while (s) {
-        if (-1 == mfile(3,head,fp)) break;
+        if (-1 == mfile(3, head, fp))
+            break;
 
-        if (strncmp(head,"ID3",3) != 0) {
-            debug("No ID3v2 tag found: %s\n",path);
+        if (strncmp(head, "ID3", 3) != 0) {
+            debug("No ID3v2 tag found: %s\n", path);
             errno = EINVAL;
             goto mbid_err_exit;
         }
 
-        if (-1 == mfile(2,version,fp)) break;
+        if (-1 == mfile(2, version, fp))
+            break;
         int version_major = (int)version[0];
         if (version_major == 2) {
-            debug("ID3v2.2.0 does not support MBIDs: %s\n",path);
+            debug("ID3v2.2.0 does not support MBIDs: %s\n", path);
             errno = EINVAL;
             goto mbid_err_exit;
         }
         if (version_major != 3 && version_major != 4) {
-            debug("Unsupported ID3 version: %d",version_major);
+            debug("Unsupported ID3 version: %d", version_major);
             errno = EINVAL;
             goto mbid_err_exit;
         }
 
-        if (-1 == mfile(1,flag,fp)) break;
+        if (-1 == mfile(1, flag, fp))
+            break;
         if ((unsigned int)flag[0] & 0x00000040) {
             debug("Extended header found.\n");
             if (version[0] == 4) {
-                if (-1 == mfile(4,size_extended,fp)) break;
+                if (-1 == mfile(4, size_extended, fp))
+                    break;
                 extended_size = to_syncsafe(size_extended);
             } else {
-                if (-1 == mfile(4,size_extended,fp)) break;
+                if (-1 == mfile(4, size_extended, fp))
+                    break;
                 extended_size = to_integer(size_extended);
             }
-            debug("Extended header size: %d\n",extended_size);
-            fseek(fp,extended_size,SEEK_CUR);
+            debug("Extended header size: %d\n", extended_size);
+            fseek(fp, extended_size, SEEK_CUR);
         }
-    
-        if (-1 == mfile(4,size,fp)) break;
+
+        if (-1 == mfile(4, size, fp))
+            break;
         tag_size = to_syncsafe(size);
-        debug("Tag size: %d\n",tag_size);
+        debug("Tag size: %d\n", tag_size);
 
         while (1) {
             if (ftell(fp) > tag_size || ftell(fp) > 1048576) {
                 errno = EFBIG;
                 goto mbid_err_exit;
             }
-            if (-1 == mfile(4,frame_bytes,fp)) goto mbid_err_exit; // frame ID
+            if (-1 == mfile(4, frame_bytes, fp))
+                goto mbid_err_exit; // frame ID
 
             if (frame_bytes[0] == 0x00) {
                 errno = EINVAL;
                 goto mbid_err_exit;
             }
             if (version_major == 4) {
-                if (-1 == mfile(4,frame_header,fp)) goto mbid_err_exit;
+                if (-1 == mfile(4, frame_header, fp))
+                    goto mbid_err_exit;
                 frame_size = to_syncsafe(frame_header);
             } else {
-                if (-1 == mfile(4,frame_header,fp)) goto mbid_err_exit;
+                if (-1 == mfile(4, frame_header, fp))
+                    goto mbid_err_exit;
                 frame_size = to_integer(frame_header);
             }
             if (frame_size <= 0 || frame_size >= tag_size) {
-                debug("Bad frame size %d\n",frame_size);
+                debug("Bad frame size %d\n", frame_size);
                 goto mbid_err_exit;
             }
-   
-            fseek(fp,2,SEEK_CUR); // 2 flag bytes
-            debug("Reading %d bytes from %s\n",frame_size,frame_bytes);
+
+            fseek(fp, 2, SEEK_CUR); // 2 flag bytes
+            debug("Reading %d bytes from %s\n", frame_size, frame_bytes);
 
             // Frame size check keeps us from blowing the stack
-            if (strncmp(frame_bytes,"UFID", 4) == 0 && frame_size <= 1024) {
+            if (strncmp(frame_bytes, "UFID", 4) == 0 && frame_size <= 1024) {
                 char frame_data[frame_size];
-                if (-1 ==  mfile(frame_size,frame_data,fp)) goto mbid_err_exit;
-                if (frame_size >= 59 && strncmp(frame_data,"http://musicbrainz.org",22) == 0) {
-                    char *tmbid = frame_data;
+                if (-1 == mfile(frame_size, frame_data, fp))
+                    goto mbid_err_exit;
+                if (frame_size >= 59 && strncmp(frame_data, "http://musicbrainz.org", 22) == 0) {
+                    char* tmbid = frame_data;
                     tmbid = frame_data + 23;
-                    strncpy(mbid,tmbid,MBID_BUFFER_SIZE-1);
-                    mbid[MBID_BUFFER_SIZE-1] = 0x00;
+                    strncpy(mbid, tmbid, MBID_BUFFER_SIZE - 1);
+                    mbid[MBID_BUFFER_SIZE - 1] = 0x00;
                     fclose(fp);
                     return 0;
                 }
             }
-            
-            fseek(fp,frame_size,SEEK_CUR);
+
+            fseek(fp, frame_size, SEEK_CUR);
         }
     }
-    
+
 mbid_err_exit:
     if (fp)
         fclose(fp);
-    debug("MBID not found: %s\n",path);
+    debug("MBID not found: %s\n", path);
     return -1;
-
 }
 
 #ifdef MBID_DEBUG
-int main(int argc, const char *argv[]) {
+int main(int argc, const char* argv[])
+{
 
     char mbid[MBID_BUFFER_SIZE];
 
-    if (parse_mbid(argv[1],mbid) == 0) {
+    if (parse_mbid(argv[1], mbid) == 0) {
         debug("File: %s\nMBID: %s\n\n", argv[1], mbid);
     }
-
 }
 #endif
